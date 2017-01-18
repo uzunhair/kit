9390f1982414aae1f1275e2d027a1c01edafa3ab
atid <uzunhair@yandex.ru>
2017-01-18 14:09:49 +0300

    Обновлен стиль виджета Список контента list_featured

--- templates/kit/controllers/content/widgets/list/list_featured.tpl.php
+++ templates/kit/controllers/content/widgets/list/list_featured.tpl.php
@@ -1,6 +1,7 @@
 <?php if ($items){ ?>
 
-    <div class="widget_content_list featured">
+    <div class="widget_content_list featured row">
+        <?php $y = ''; ?>
         <?php foreach($items as $item) { ?>
 
             <?php
@@ -16,19 +17,22 @@
                     $url = '';
                 }
             ?>
+            <?php if ($is_first) { ?>
+                <div class="col-12 col-lg-5 mb-3">
+            <?php } ?>
 
-            <div class="item <?php if ($is_first) { ?>item-first<?php } ?>">
+            <div class="item <?php echo ($is_first) ? 'item-first card' : 'media mt-next-3'; ?>">
                 <?php if ($image) { ?>
                     <?php if ($is_first) { ?>
-                        <div class="image">
-                            <?php if ($url) { ?>
-                                <a style="background-image:url('<?php echo html_image_src($image, $size, true); ?>')" href="<?php echo $url; ?>"></a>
-                            <?php } else { ?>
-                                <div style="background-image:url('<?php echo html_image_src($image, $size, true); ?>')"></div>
-                            <?php } ?>
-                        </div>
+                        <?php if ($url) { ?>
+                            <a href="<?php echo $url; ?>">
+                                <img class="card-img-top" src="<?php echo html_image_src($image, $size, true); ?>" alt="<?php html($item['title']); ?>">
+                            </a>
+                        <?php } else { ?>
+                            <img class="card-img-top" src="<?php echo html_image_src($image, $size, true); ?>" alt="<?php html($item['title']); ?>">
+                        <?php } ?>
                     <?php } else { ?>
-                        <div class="image">
+                        <div class="d-flex mr-3 image">
                             <?php if ($url) { ?>
                                 <a href="<?php echo $url; ?>"><?php echo html_image($image, $size, $item['title']); ?></a>
                             <?php } else { ?>
@@ -37,33 +41,8 @@
                         </div>
                     <?php } ?>
                 <?php } ?>
-                <div class="info">
-                    <?php if ($is_first && $is_show_details) { ?>
-                        <div class="details">
-                            <span class="author">
-                                <a href="<?php echo href_to('users', $item['user']['id']); ?>"><?php html($item['user']['nickname']); ?></a>
-                                <?php if ($item['parent_id']){ ?>
-                                    <?php echo LANG_WROTE_IN_GROUP; ?>
-                                    <a href="<?php echo rel_to_href($item['parent_url']); ?>"><?php html($item['parent_title']); ?></a>
-                                <?php } ?>
-                            </span>
-                            <span class="date">
-                                <?php html(string_date_age_max($item['date_pub'], true)); ?>
-                            </span>
-                            <?php if($ctype['is_comments']){ ?>
-                                <span class="comments">
-                                    <?php if ($url) { ?>
-                                        <a href="<?php echo $url . '#comments'; ?>" title="<?php echo LANG_COMMENTS; ?>">
-                                            <?php echo intval($item['comments']); ?>
-                                        </a>
-                                    <?php } else { ?>
-                                        <?php echo intval($item['comments']); ?>
-                                    <?php } ?>
-                                </span>
-                            <?php } ?>
-                        </div>
-                    <?php } ?>
-                    <div class="title">
+                <div class="info  <?php if ($is_first) { ?> card-block <?php } ?>">
+                    <div class="title<?php if ($is_first) { ?> card-title h4<?php } ?>">
                         <?php if ($url) { ?>
                             <a href="<?php echo $url; ?>"><?php html($item['title']); ?></a>
                         <?php } else { ?>
@@ -83,24 +62,26 @@
                         </div>
                     <?php } ?>
                     <?php if ($is_first && !$is_private) { ?>
-                        <div class="read-more">
-                            <a href="<?php echo $url; ?>"><?php echo LANG_MORE; ?></a>
+                        <div class="read-more mt-3">
+                            <a class="btn btn-primary" href="<?php echo $url; ?>"><?php echo LANG_MORE; ?></a>
                         </div>
                     <?php } ?>
                     <?php if (!$is_first && $is_show_details) { ?>
-                        <div class="details">
-                            <span class="author">
-                                <a href="<?php echo href_to('users', $item['user']['id']); ?>"><?php html($item['user']['nickname']); ?></a>
+                        <div class="details text-muted">
+                            <span class="list-inline-item author">
+                                <a href="<?php echo href_to('users', $item['user']['id']); ?>"><i class="fa fa-user"></i> <?php html($item['user']['nickname']); ?></a>
                                 <?php if ($item['parent_id']){ ?>
                                     <?php echo LANG_WROTE_IN_GROUP; ?>
                                     <a href="<?php echo rel_to_href($item['parent_url']); ?>"><?php html($item['parent_title']); ?></a>
                                 <?php } ?>
                             </span>
-                            <span class="date">
+                            <span class="list-inline-item date">
+                                <i class="fa fa-calendar"></i>
                                 <?php html(string_date_age_max($item['date_pub'], true)); ?>
                             </span>
                             <?php if($ctype['is_comments']){ ?>
-                                <span class="comments">
+                                <span class="list-inline-item comments">
+                                    <i class="fa fa-comment-o"></i>
                                     <?php if ($url) { ?>
                                         <a href="<?php echo $url . '#comments'; ?>" title="<?php echo LANG_COMMENTS; ?>">
                                             <?php echo intval($item['comments']); ?>
@@ -113,7 +94,42 @@
                         </div>
                     <?php } ?>
                 </div>
+                <?php if ($is_first && $is_show_details) { ?>
+                    <div class="details text-muted card-footer">
+                            <span class="list-inline-item author">
+                                <a href="<?php echo href_to('users', $item['user']['id']); ?>"><i class="fa fa-user"></i> <?php html($item['user']['nickname']); ?></a>
+                                <?php if ($item['parent_id']){ ?>
+                                    <?php echo LANG_WROTE_IN_GROUP; ?>
+                                    <a href="<?php echo rel_to_href($item['parent_url']); ?>"><?php html($item['parent_title']); ?></a>
+                                <?php } ?>
+                            </span>
+                        <span class="list-inline-item date">
+                                <i class="fa fa-calendar"></i>
+                            <?php html(string_date_age_max($item['date_pub'], true)); ?>
+                            </span>
+                        <?php if($ctype['is_comments']){ ?>
+                            <span class="list-inline-item comments">
+                                    <i class="fa fa-comment-o"></i>
+                                <?php if ($url) { ?>
+                                    <a href="<?php echo $url . '#comments'; ?>" title="<?php echo LANG_COMMENTS; ?>">
+                                            <?php echo intval($item['comments']); ?>
+                                        </a>
+                                <?php } else { ?>
+                                    <?php echo intval($item['comments']); ?>
+                                <?php } ?>
+                                </span>
+                        <?php } ?>
+                    </div>
+                <?php } ?>
             </div>
+            <?php if ($is_first) { ?>
+                </div>
+                <div class="col-12 col-lg-7">
+            <?php } ?>
+
+            <?php $y++; if ($y == count($items)){ ?>
+                </div>
+            <?php } ?>
 
         <?php } ?>
     </div>
