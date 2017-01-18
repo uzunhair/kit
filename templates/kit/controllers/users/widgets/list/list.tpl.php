748af62fac36dbbe65d2e8d0adc4e7b93ad5f9c0
atid <uzunhair@yandex.ru>
2017-01-18 03:23:47 +0300

    Переделаны виджеты пользователя

--- templates/kit/controllers/users/widgets/list/list.tpl.php
+++ templates/kit/controllers/users/widgets/list/list.tpl.php
@@ -15,17 +15,18 @@
                 </div>
             <?php } ?>
         <?php } else { ?>
+            <div class="d-flex flex-wrap">
             <?php foreach($profiles as $profile) { ?>
 
                 <?php $url = href_to('users', $profile['id']); ?>
-
-                <div class="mb-3">
-                    <a class="d-flex mr-3" href="<?php echo $url; ?>" title="<?php html($profile['nickname']); ?>"><?php echo html_avatar_image($profile['avatar'], 'small', $profile['nickname']); ?></a>
+                <div class="px-2">
+                    <a href="<?php echo $url; ?>" title="<?php html($profile['nickname']); ?>"><?php echo html_avatar_image($profile['avatar'], 'small', $profile['nickname']); ?></a>
                     <div class="media-body align-self-center">
-                        <a href="<?php echo $url; ?>"><?php html($profile['nickname']); ?></a>
+                        <a class="text-truncate" href="<?php echo $url; ?>"><?php html($profile['nickname']); ?></a>
                     </div>
                 </div>
             <?php } ?>
+            </div>
         <?php } ?>
     </div>
 
