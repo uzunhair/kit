4eddb78b9f24316b6c340745679b0ea589b0cd83
atid <uzunhair@yandex.ru>
2017-01-29 21:52:35 +0300

    Выравнены ссылки в заголовках виджета

--- templates/kit/widgets/wrapper.tpl.php
+++ templates/kit/widgets/wrapper.tpl.php
@@ -1,10 +1,10 @@
 <div class="widget<?php if ($widget['class_wrap']) { ?> <?php echo ltrim($widget['class_wrap'], '.');  } ?>">
 
     <?php if ($widget['title'] && $is_titles){ ?>
-        <div class="title<?php if ($widget['class_title']) { ?> <?php echo ltrim($widget['class_title'], '.');  } ?>">
-            <?php echo $widget['title']; ?>
+        <div class="title d-flex align-items-center <?php if ($widget['class_title']) { ?> <?php echo ltrim($widget['class_title'], '.');  } ?>">
+            <span class="mr-auto"><?php echo $widget['title']; ?></span>
             <?php if (!empty($widget['links'])) { ?>
-                <div class="links">
+                <div class="links font-size-base">
                     <?php $links = string_parse_list($widget['links']); ?>
                     <?php foreach($links as $link){ ?>
                         <a href="<?php echo (mb_strpos($link['value'], 'http://')===0) ? $link['value'] : href_to($link['value']); ?>"><?php echo $link['id']; ?></a>
