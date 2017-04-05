3e52437e22b6bc65bc4d98119d706471bb00b249
atid <uzunhair@yandex.ru>
2017-04-05 08:18:04 +0300

    Удаление не используемых файлов виджетов

--- system/languages/ru/controllers/kitdeveloper/widgets/kitcarousel.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('LANG_WD_KITCAROUSEL_DATASET',  'Применить фильтры из набора');
-define('LANG_WD_KITCAROUSEL_IMAGE',    'Поле изображения');
-define('LANG_WD_KITCAROUSEL_TEASER',   'Поле краткого описания');
-define('LANG_WD_KITCAROUSEL_DETAILS',   'Показывать автора, дату и количество комментариев');
-define('LANG_WD_KITCAROUSEL_WIDGET_TYPE',    'Режим виджета');
-define('LANG_WD_KITCAROUSEL_WIDGET_TYPE1',    'Выводить обычный список');
-define('LANG_WD_KITCAROUSEL_WIDGET_TYPE2',    'Выводить список похожих');
